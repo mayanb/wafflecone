1c13cf343bb769e3bda4f5b79aafabea832114c8
Ishita Prasad <ishitap@stanford.edu>
2016-11-30 10:35:52 -0800

    added process and product list urls

--- ics/urls.py
+++ ics/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     url(r'^tasks/(?P<pk>[0-9]+)/$', views.TaskDetail.as_view()),
     url(r'^items/$', views.ItemList.as_view()),
     url(r'^items/(?P<pk>[0-9]+)/$', views.ItemDetail.as_view()),
+    url(r'^processes/$', views.ProcessList.as_view()),
+    url(r'^products/$', views.ProductList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- ics/views.py
+++ ics/views.py
@@ -1,14 +1,13 @@
 from rest_framework import status
 from rest_framework.response import Response
 from ics.models import *
-from ics.serializers import NestedTaskSerializer
-from ics.serializers import NestedItemSerializer
+from ics.serializers import *
 from rest_framework import generics
 from django.shortcuts import get_object_or_404, render
 import django_filters
 from rest_framework.filters import OrderingFilter
 from rest_framework.views import APIView
-
+ 
 
 class TaskList(generics.ListCreateAPIView):
   queryset = Task.objects.all()
@@ -31,6 +30,14 @@ class ItemDetail(generics.RetrieveUpdateDestroyAPIView):
   queryset = Item.objects.all()
   serializer_class = NestedItemSerializer
   filter_fields = ('item_qr', 'creating_task')
+
+class ProcessList(generics.ListCreateAPIView):
+  queryset = ProcessType.objects.all()
+  serializer_class = ProcessTypeSerializer
+
+class ProductList(generics.ListCreateAPIView):
+  queryset = ProductType.objects.all()
+  serializer_class = ProductTypeSerializer
 
 def index(request):
   return HttpResponse("Hello, world. You're at the ics index.")
