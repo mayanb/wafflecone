7f3b27aba2600540327f656b741879c23ed8005c
Ishita Prasad <ishitap@stanford.edu>
2017-01-11 08:37:11 -0800

    label index code now in db instead of app

--- ics/models.py
+++ ics/models.py
@@ -26,18 +26,49 @@ class ProductType(models.Model):
   def __str__(self):
     return self.name
 
+
 class Task(models.Model):
   process_type = models.ForeignKey(ProcessType, on_delete=models.CASCADE)
   product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
   label = models.CharField(max_length=20)  
+  label_index = models.PositiveSmallIntegerField(default=0)
+  display = models.CharField(max_length=25, blank=True)
+  custom_display = models.CharField(max_length=25, blank=True)
   created_by = models.ForeignKey(User, on_delete=models.CASCADE)
   is_open = models.BooleanField(default=True)
+
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
-  label_index = models.PositiveSmallIntegerField(default=0)
 
   def __str__(self):
-    return self.label
+    if not self.custom_display:
+      return self.display
+    else:
+      return self.custom_display
+
+  def save(self, *args, **kwargs):
+    # get the # of tasks with the same name & made on the same date this year
+    q = ( 
+      Task.objects.filter(label=self.label)
+        .filter(created_at__startswith=str(datetime.now().year))
+        .order_by('-label_index')
+      )
+
+    # tentatively, label_index is the # of items with same name
+    self.label_index = len(q)
+
+    # if there are other items with the same name, then actually get the highest label_index 
+    # and set our label index to that + 1
+    if self.label_index > 0:
+      self.label_index = q[0].label_index + 1
+      self.display = "-".join([self.label, str(self.label_index)])
+    else:
+      self.display = self.label
+
+    super(Task, self).save(*args, **kwargs)
+
+
+
 
 class Item(models.Model):
   item_qr = models.CharField(max_length=100)
@@ -65,5 +96,5 @@ class Attribute(models.Model):
 class TaskAttribute(models.Model):
   attribute = models.ForeignKey(Attribute, on_delete=models.CASCADE)
   task = models.ForeignKey(Task, on_delete=models.CASCADE)
-  value = models.CharField(max_length=50)
+  value = models.CharField(max_length=50, blank=True)
   updated_at = models.DateTimeField(auto_now=True)
