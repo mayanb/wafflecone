722acc16753aaced0f79b1bab203c7588efd4d74
Ishita Prasad <ishitap@stanford.edu>
2017-03-27 14:24:11 -0700

    commented out hello line

--- logistics/urls.py
+++ logistics/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 import hello.views
 
 urlpatterns = [
-    url(r'^$', hello.views.index, name='index'),
+    #url(r'^$', hello.views.index, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^ics/', include('ics.urls')),
     url(r'^qr/', include('qr.urls')),
