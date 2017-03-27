ac7114feed84443182558fb6e2ae480ed704d15f
Ishita Prasad <ishitap@stanford.edu>
2017-03-27 14:25:31 -0700

    commented out hello line

--- logistics/urls.py
+++ logistics/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 admin.autodiscover()
 
-import hello.views
+#import hello.views
 
 urlpatterns = [
     #url(r'^$', hello.views.index, name='index'),
