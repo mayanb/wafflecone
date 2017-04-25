9aebbfe8b2e9f0093bbb366c45f7b3bf3122356d
Ishita Prasad <ishitap@stanford.edu>
2017-04-25 11:46:11 -0700

    qr view

--- qr/urls.py
+++ qr/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='qr'),
     url(r'^codes/$', views.codes, name='codes'),
+    url(r'^images/$', views.images, name='images'),
 ]
--- qr/views.py
+++ qr/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from uuid import uuid4
 from django.http import HttpResponse
+from PIL import Image, ImageChops
+import qrcode
 
 def index(request):
   return render(request, 'qr/index.html')
@@ -13,3 +15,49 @@ def codes(request):
 	for i in range(count):
 		uuids.append("dande.li/ics/" + str(uuid4()))
 	return HttpResponse('\n'.join(uuids))
+
+
+def images(request):
+  ROWS = int(request.GET.get('rows'))
+  COLS = int(request.GET.get('cols'))
+  
+  # all sizes are in pixels
+  QR_SIZE = 100
+  STICKER_SIZE = 200     
+  X_OFFSET = 10
+  Y_OFFSET = 10
+  X_SPACING = 10
+  Y_SPACING = 10
+
+  count = ROWS * COLS
+  width = (X_OFFSET * 2) + (STICKER_SIZE * ROWS) + (X_SPACING * (ROWS-1))
+  height = (Y_OFFSET * 2) + (STICKER_SIZE * COLS) + (Y_SPACING * (COLS-1))
+  diff = (STICKER_SIZE - QR_SIZE)/2
+
+  img = Image.new("RGB", (width, height), "white")
+
+  for i in range(count):
+    code_text = "dande.li/ics/" + str(uuid4())
+    qr = qrcode.QRCode(
+      version=None,
+      error_correction=qrcode.constants.ERROR_CORRECT_L,
+      box_size=10,
+      border=0,
+    )
+    qr.add_data(code_text)
+    qr.make(fit=True)
+    qr_img = qr.make_image().resize((QR_SIZE, QR_SIZE))
+
+    x = i/COLS
+    y = i % COLS 
+    x_pos = X_OFFSET + x*STICKER_SIZE + x*X_SPACING + diff
+    y_pos = Y_OFFSET + y*STICKER_SIZE + y*Y_SPACING + diff
+    img.paste(qr_img, (x_pos, y_pos, x_pos + QR_SIZE, y_pos + QR_SIZE))
+
+  brown = Image.new("RGB", (width, height), "#3c2415")
+  final = ImageChops.lighter(brown, img)
+  response = HttpResponse(content_type="image/png")
+  final.save(response, "PNG")
+  return response
+
+
