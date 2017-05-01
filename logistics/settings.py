6ee8391c64c489614e0298686ffc118647ddee88
Ishita Prasad <ishitap@stanford.edu>
2017-05-01 11:17:44 -0700

    remote custom staticfiles

--- logistics/settings.py
+++ logistics/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
-    'logistics',
     'rest_framework_docs',
     'rest_framework',
     'ics',
