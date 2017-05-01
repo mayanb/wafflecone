ce06a188d95f311371aa6df4473f401feffbc4d2
Ishita Prasad <ishitap@stanford.edu>
2017-05-01 11:18:45 -0700

    ??

--- logistics/settings.py
+++ logistics/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'logistics.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'debug': True,
