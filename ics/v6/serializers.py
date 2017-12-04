0f88f5dbfaf5d7ed8549b338cd95851549622a41
Maya Balakrishnan <mayanb@gmail.com>
2017-12-04 11:15:53 -0800

    added process type to edit task serializer in v6

--- ics/v6/serializers.py
+++ ics/v6/serializers.py
@@ -78,9 +78,10 @@ class EditTaskSerializer(serializers.ModelSerializer):
 	created_at = serializers.DateTimeField(read_only=True)
 	display = serializers.CharField(source='*', read_only=True)
 	process_type = serializers.IntegerField(source='process_type.id', read_only=True)
+	product_type = serializers.IntegerField(source='product_type.id', read_only=True)
 	class Meta:
 		model = Task
-		fields = ('id', 'is_open', 'custom_display', 'is_trashed', 'is_flagged', 'display', 'process_type', 'created_at', 'search')
+		fields = ('id', 'is_open', 'custom_display', 'is_trashed', 'is_flagged', 'display', 'process_type', 'product_type', 'created_at', 'search')
 
 class BasicItemSerializer(serializers.ModelSerializer):
 	is_used = serializers.CharField(read_only=True)
