2b6f511ee46b3b450fdc23ef71670a658ac96bec
Ishita Prasad <ishita@usepolymer.com>
2018-02-14 13:14:33 -0800

    added product type filter

--- graphs/v2/views.py
+++ graphs/v2/views.py
@@ -19,24 +19,31 @@ mock_data = {
 @api_view(['GET'])
 def production_actuals(request):
 	process_type = request.GET.get('process_type', mock_data['process_type'])
+	product_types = request.GET.get('product_types', None)
 	start = request.GET.get('start', mock_data['start'])
 	end = request.GET.get('end', mock_data['end'])
 
-	queryset = get_output_by_month(start, end, process_type)
+	queryset = get_output_by_month(start, end, process_type, product_types)
 	serializer = ProductionActualsSerializer(queryset, many=True)
 	return Response(serializer.data)
 
-def get_output_by_month(start, end, process_type):
+def get_output_by_month(start, end, process_type, product_types):
 	dt = datetime
 	tz = pytz.timezone('America/Los_Angeles')
 	startDate = dt.strptime(start, dateformat)
 	endDate = dt.strptime(end, dateformat)
 
-	return Task.objects.filter(
+	t = Task.objects.filter(
 		is_trashed=False,
 		process_type=process_type,
 		created_at__range=(startDate, endDate)
-	).annotate(
+	)
+
+	if product_types is not None:
+		product_types = product_types.split(',')
+		t = t.filter(product_type__in=product_types)
+
+	return t.annotate(
 		bucket=TruncMonth('created_at', tzinfo=tz)
 	).values('bucket').annotate(
 		total_amount=Sum('items__amount')
