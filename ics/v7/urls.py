61e5fdac50d4944176c13dc486dfa1946d07debe
Addison Leong <addisonleong728@gmail.com>
2018-03-01 15:21:54 -0800

    Refactor v8 to v7

--- ics/v7/urls.py
+++ ics/v7/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from ics.v7 import views
+from ics.v8 import views
 from django.contrib.auth import views as auth_views
 
 import oauth2client.contrib.django_util.site as django_util_site
