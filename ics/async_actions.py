6ecc8690dcd989fdfe29482839716d18f3a155cd
Ishita Prasad <ishita@usepolymer.com>
2018-04-11 21:42:48 -0700

    yayyy got async actions working

---
+++ ics/async_actions.py
@@ -0,0 +1,9 @@
+from ics.models import *
+from zappa.async import task
+
+@task
+def update_task_search_vector(**kwargs):
+	tasks = Task.objects.with_documents().filter(**kwargs).distinct()
+	for task in tasks:
+		task.search = task.document
+		task.save(update_fields=['search'])
