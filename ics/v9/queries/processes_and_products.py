f0426e9e35a8eb6a21a34d097963ca907bda510e
Ishita Prasad <ishita@usepolymer.com>
2018-04-25 13:21:50 -0700

    process and product search filters

---
+++ ics/v9/queries/processes_and_products.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.models import User
+from django.db.models import F, Q, Count, Case, When, Min, Value, Subquery, OuterRef, Sum, DecimalField
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from ics.models import *
+import datetime
+
+def process_search(query_params):
+	return filter_results(
+		ProcessType.objects.all(), 
+		query_params
+	).select_related(
+		'created_by', 'team_created_by'
+	).prefetch_related('attribute_set')
+
+def product_search(query_params):
+	return filter_results(
+		ProductType.objects.all(), 
+		query_params
+	).annotate(
+		last_used=Max('task__created_at')
+	).select_related('created_by')
+
+def filter_results(queryset, query_params):
+	queryset = queryset.filter(is_trashed=False)
+	team = query_params.get('team', None)
+	if team is not None:
+		queryset = queryset.filter(process_type__team_created_by=team)
+
+	f = query_params.get('filter', None)
+	if f is not None:
+		print(f)
+		queryset = queryset.filter(Q(search=SearchQuery(f)) | Q(name__istartswith=f) | Q(code__istartswith=f))
+	return queryset
