928cae21af59f2c3c4bda4554f540a70bd60838b
David Mora <davidnmora@gmail.com>
2018-05-03 10:30:46 -0700

    Finish command

--- ics/management/commands/user_attr_to_text_attr.py
+++ ics/management/commands/user_attr_to_text_attr.py
@@ -1,13 +1,12 @@
-import csv
 from ics.models import *
-from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
 class Command(BaseCommand):
 	help = 'In order to release the user attributes, change existing ones to text so we dont throw people off'
 
-	def handle(self):
+	def handle(self, *args, **options):
 		user_attributes = Attribute.objects.filter(datatype='USER')
+		msg = 'Successfully changed all ' + str(user_attributes.count()) + ' Process user attributes to text attributes'
 		user_attributes.update(datatype='TEXT')
-		msg = 'Successfully changed all ' + str(len(user_attributes)) + ' Process user attributes to text attributes'
+
 		self.stdout.write(self.style.SUCCESS(msg))
