ec2d4111f48c84171a7395990541e29910503efd
Steven Guichard <steven@usepolymer.com>
2018-05-03 15:59:15 -0700

    Added logging to command

--- ics/management/commands/set_timezones.py
+++ ics/management/commands/set_timezones.py
@@ -9,8 +9,10 @@ class Command(BaseCommand):
 		team = Team.objects.get(name=name)
 		team.timezone = pytz.timezone(timezone).zone
 		team.save()
+		self.stdout.write("Updated timezone of " + name + " to " + timezone)
 
 	def handle(self, *args, **options):
 		self.set_timezone('soma', 'US/Eastern')
 		self.set_timezone('teammindo', 'US/Eastern')
+		self.stdout.write(self.style.SUCCESS('Timezones successfully updated'))
 
