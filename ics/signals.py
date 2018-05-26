6712b81e2e6643448d276dec8fe4e41b45c6afaf
maya <mayanb@gmail.com>
2018-05-25 17:54:12 -0700

    asdf

--- ics/signals.py
+++ ics/signals.py
@@ -21,7 +21,8 @@ def taskattribute_changed(sender, instance, **kwargs):
 
 @receiver(post_save, sender=Task)
 def task_changed(sender, instance, **kwargs):
-	update_task_descendents_flag_number(pk=instance.id)
+	kwargs = { 'pk' : instance.id }
+	update_task_descendents_flag_number(**kwargs)
 
 	#Don't create duplicate alerts after updating task search field
 	# if 'update_fields' not in kwargs or not kwargs['update_fields'] or 'search' not in kwargs['update_fields']:
