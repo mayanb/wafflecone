d6cbb10e8d4137d2364cb66a49c4af6960b01ecc
aditya <aditya@usepolymer.com>
2018-07-09 13:30:06 -0700

    update duplicate_process test

--- ics/tests/test_process_types.py
+++ ics/tests/test_process_types.py
@@ -33,54 +33,57 @@ class TestProcessTypes(APITestCase):
 		self.assertEqual(process_type.default_amount, 425)
 		self.assertEqual(process_type.unit, 'kg')
 
-	# def test_duplicate_process_type_to_duplicate(self):
-	# 	process_type_to_duplicate = ProcessTypeFactory(
-	# 		created_by=self.user_profile_two.user,
-	# 		team_created_by=self.user_profile_two.team,
-	# 		name='old-process-name',
-	# 		code='old-process-code',
-	# 		icon='old-icon',
-	# 		output_desc='old-output_desc',
-	# 		default_amount=111,
-	# 		unit='old-unit',
-	# 		is_trashed=False,
-	# 	)
-	# 	num_attributes = 4
-	# 	for i in range(num_attributes):
-	# 		AttributeFactory(name=i, process_type=process_type_to_duplicate)
-	# 	url = reverse('process_duplicate')
-	# 	data = {
-	# 		'created_by': self.user_profile.user.id,
-	# 		'team_created_by': self.user_profile.team.id,
-	# 		'name': 'new-process-name',
-	# 		'code': 'new-process-code',
-	# 		'duplicate_id': process_type_to_duplicate.id,
-	# 		'icon': 'new-icon',
-	# 		'output_desc': 'new-output_desc',
-	# 		'default_amount': 333,
-	# 		'unit': 'new-unit',
-	# 		'is_trashed': False,
-	# 	}
-	# 	response = self.client.post(url, data)
-	# 	self.assertEqual(response.status_code, 201)
-	# 	duplicate_process = ProcessType.objects.get(id=response.data['id'])
+	def test_duplicate_process_type_to_duplicate(self):
+		process_type_to_duplicate = ProcessTypeFactory(
+			created_by=self.user_profile_two.user,
+			team_created_by=self.user_profile_two.team,
+			name='old-process-name',
+			code='old-process-code',
+			icon='old-icon',
+			output_desc='old-output_desc',
+			default_amount=111,
+			unit='old-unit',
+			is_trashed=False,
+			category='wip'
+		)
+		num_attributes = 4
+		for i in range(num_attributes):
+			AttributeFactory(name=i, process_type=process_type_to_duplicate)
+		url = reverse('process_duplicate')
+		data = {
+			'created_by': self.user_profile.user.id,
+			'team_created_by': self.user_profile.team.id,
+			'name': 'new-process-name',
+			'code': 'new-process-code',
+			'duplicate_id': process_type_to_duplicate.id,
+			'icon': 'new-icon',
+			'output_desc': 'new-output_desc',
+			'default_amount': 333,
+			'unit': 'new-unit',
+			'is_trashed': False,
+			'category':'wip',
+		}
+		response = self.client.post(url, data)
+		self.assertEqual(response.status_code, 201)
+		duplicate_process = ProcessType.objects.get(id=response.data['id'])
 
-	# 	# Verify duplicate process contains all properties assigned to it via the POST request
-	# 	self.assertEqual(data.get('created_by'), duplicate_process.created_by.id)
-	# 	self.assertEqual(data.get('team_created_by'), duplicate_process.team_created_by.id)
-	# 	self.assertEqual(data.get('name'), duplicate_process.name)
-	# 	self.assertEqual(data.get('code'), duplicate_process.code)
-	# 	self.assertEqual(data.get('icon'), duplicate_process.icon)
-	# 	self.assertEqual(data.get('output_desc'), duplicate_process.output_desc)
-	# 	self.assertEqual(data.get('default_amount'), duplicate_process.default_amount)
-	# 	self.assertEqual(data.get('unit'), duplicate_process.unit)
-	# 	self.assertEqual(data.get('is_trashed'), duplicate_process.is_trashed)
+		# Verify duplicate process contains all properties assigned to it via the POST request
+		self.assertEqual(data.get('created_by'), duplicate_process.created_by.id)
+		self.assertEqual(data.get('team_created_by'), duplicate_process.team_created_by.id)
+		self.assertEqual(data.get('name'), duplicate_process.name)
+		self.assertEqual(data.get('code'), duplicate_process.code)
+		self.assertEqual(data.get('icon'), duplicate_process.icon)
+		self.assertEqual(data.get('output_desc'), duplicate_process.output_desc)
+		self.assertEqual(data.get('default_amount'), duplicate_process.default_amount)
+		self.assertEqual(data.get('unit'), duplicate_process.unit)
+		self.assertEqual(data.get('is_trashed'), duplicate_process.is_trashed)
+		self.assertEqual(data.get('category'), duplicate_process.category)
 
-	# 	# Verify that copied attributes are the same as the process type being copied
-	# 	old_attributes = process_type_to_duplicate.attribute_set.all()
-	# 	new_attributes = duplicate_process.attribute_set.all()
-	# 	for i in range(num_attributes):
-	# 		self.assertEqual(old_attributes[i].name, new_attributes[i].name)
+		# Verify that copied attributes are the same as the process type being copied
+		old_attributes = process_type_to_duplicate.attribute_set.all()
+		new_attributes = duplicate_process.attribute_set.all()
+		for i in range(num_attributes):
+			self.assertEqual(old_attributes[i].name, new_attributes[i].name)
 
 	def test_list_process_types(self):
 		ProcessTypeFactory(
