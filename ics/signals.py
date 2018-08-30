01009267ee39b61ff1eae6e0babf318b1679eaba
David Mora <davidnmora@gmail.com>
2018-08-30 11:29:29 -0700

    Fix flagged tasks bug (caused by duplicate function names, whoops! :whale: )

--- ics/signals.py
+++ ics/signals.py
@@ -22,6 +22,7 @@ def taskattribute_changed(sender, instance, **kwargs):
 
 @receiver(post_save, sender=Task)
 def task_changed(sender, instance, **kwargs):
+	print('taskchanged')
 	kwargs = { 'pk' : instance.id }
 	update_task_descendents_flag_number(**kwargs)
 
@@ -31,8 +32,7 @@ def task_changed(sender, instance, **kwargs):
 
 
 @receiver(pre_save, sender=Task)
-def task_changed(sender, instance, **kwargs):
-	print("pre_save called on Task")
+def task_changed_pre_save(sender, instance, **kwargs):
 	if instance.is_trashed:
 		task_deleted_update_cost(instance.id)
 
