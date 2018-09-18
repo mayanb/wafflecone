8598972c211fba53db2d16c181be1ce2767e61a4
David Mora <davidnmora@gmail.com>
2018-09-18 15:40:31 -0700

    Fix command comments/print outs
    Should NOT instruct you to comment out signals, which are needed to update TaskIngredient.

--- ics/management/commands/delete_inputs_to_and_from_deleted_tasks.py
+++ ics/management/commands/delete_inputs_to_and_from_deleted_tasks.py
@@ -6,14 +6,18 @@ class Command(BaseCommand):
     help = 'Deleted tasks, you no need inputs!'
 
     def handle(self, *args, **options):
-        self.stdout.write('IMPORTANT: You are going to want to comment out the pre_/post_delete Input signals or this will run super slow')
+        self.stdout.write('This command deletes any inputs to/from deleted tasks.\nIt will trigger signals.py to update TaskIngredient, which was previously not updated after a task deletion')
+
         # delete Inputs to deleted children
-        Input.objects.filter(input_item__creating_task__is_trashed=True).delete()
+        count, o = Input.objects.filter(input_item__creating_task__is_trashed=True).delete()
+        self.stdout.write('Deleted %s child inputs...' % str(count))
 
         # delete Inputs from deleted parents
-        Input.objects.filter(task__is_trashed=True).delete()
+        count, o = Input.objects.filter(task__is_trashed=True).delete()
+        self.stdout.write('Deleted %s parent inputs...' % str(count))
 
         # zero cost
-        Task.objects.filter(is_trashed=True).update(cost=None, remaining_worth=None)
+        count = Task.objects.filter(is_trashed=True).update(cost=None, remaining_worth=None)
+        self.stdout.write('Zero-ed %s deleted tasks cost and remaining_worth...' % str(count))
 
         self.stdout.write(self.style.SUCCESS('Successfully deleted all inputs to/from deleted tasks. Yay!'))
