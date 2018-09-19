8fde11895dcae1390c92b6332d293cf5be66c9a6
David Mora <davidnmora@gmail.com>
2018-09-18 18:38:10 -0700

    Revert "Merge branch 'staging' into new-flagged-ancestors"
    This reverts commit b1f6d55f0f6da88bbf056b8d4887a380115c7ca1, reversing
    changes made to f3181fd5d1b658d6197341f22db556080be65c71.

--- ics/v11/queries/processes_and_products.py
+++ ics/v11/queries/processes_and_products.py
@@ -1,7 +1,6 @@
 from django.db.models import Q
 from django.contrib.postgres.search import SearchQuery
 from ics.models import *
-from django.db.models import Case, When, Value, IntegerField
 
 def process_search(query_params):
 	return filter_results(
@@ -13,7 +12,7 @@ def process_search(query_params):
 
 def product_search(query_params):
 	return filter_results(
-		ProductType.objects.all(),
+		ProductType.objects.all(), 
 		query_params
 	).annotate(
 		last_used=Max('task__created_at')
@@ -27,33 +26,9 @@ def filter_results(queryset, query_params):
 
 	f = query_params.get('filter', None)
 	if f is not None:
-		queryset = queryset.filter(Q(search=SearchQuery(f)) | Q(name__icontains=f) | Q(code__icontains=f) | Q(tags__name__icontains=f))
+		queryset = queryset.filter(Q(search=SearchQuery(f)) | Q(name__icontains=f) | Q(code__icontains=f))
 
 	process = query_params.get('process', None)
 	if process is not None:
 		queryset = queryset.filter(task__process_type=process)
-
-	tags = query_params.get('tags', None)
-	suggest_tags = stringToBoolean(query_params.get('suggestTags', None))
-	if tags is not None:
-		tag_names = tags.strip().split(',')
-		if suggest_tags is None or not suggest_tags:
-			queryset = queryset.filter(tags__name__in=tag_names)
-		else:
-			queryset = queryset.annotate(
-				suggested=Case(
-					When(tags__name__in=tag_names, then=Value(1)),
-					default=Value(0),
-					output_field=IntegerField()
-				),
-			).order_by('-suggested')
 	return queryset
-
-def stringToBoolean(boolStr):
-	if boolStr is None:
-		return None
-	if boolStr.lower() == 'true':
-		return True
-	elif boolStr.lower() == 'false':
-		return False
-	return None
