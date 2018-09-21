d12c2a745f62480e20eff1a624dad2267f42f589
David Mora <davidnmora@gmail.com>
2018-09-21 12:03:44 -0700

    handle exceptions

--- ics/management/commands/delete_inputs_to_and_from_deleted_tasks.py
+++ ics/management/commands/delete_inputs_to_and_from_deleted_tasks.py
@@ -6,18 +6,30 @@ class Command(BaseCommand):
     help = 'Deleted tasks, you no need inputs!'
 
     def handle(self, *args, **options):
-        self.stdout.write('This command deletes any inputs to/from deleted tasks.\nIt will trigger signals.py to update TaskIngredient, which was previously not updated after a task deletion')
+        try:
+            self.stdout.write('This command deletes any inputs to/from deleted tasks.\nIt will trigger signals.py to update TaskIngredient, which was previously not updated after a task deletion')
 
-        # delete Inputs to deleted children
-        count, o = Input.objects.filter(input_item__creating_task__is_trashed=True).delete()
-        self.stdout.write('Deleted %s child inputs...' % str(count))
+            # delete Inputs from deleted parents
+            inputs_from_deleted_parents = Input.objects.filter(input_item__creating_task__is_trashed=True)
+            for i in inputs_from_deleted_parents:
+                print('from_deleted_parent_input (parent, child)', i.input_item.creating_task.id, i.task.id)
+                i.delete()
+            self.stdout.write('Deleted %s child inputs...' % str(inputs_from_deleted_parents.count()))
 
-        # delete Inputs from deleted parents
-        count, o = Input.objects.filter(task__is_trashed=True).delete()
-        self.stdout.write('Deleted %s parent inputs...' % str(count))
+            # delete Inputs to deleted children
+            inputs_to_deleted_children = Input.objects.filter(task__is_trashed=True)
+            for i in inputs_to_deleted_children:
+                print('to_deleted_child_input (parent, child)', i.input_item.creating_task.id, i.task.id)
+                i.delete()
+            self.stdout.write('Deleted %s parent inputs...' % str(inputs_to_deleted_children.count()))
 
-        # zero cost
-        count = Task.objects.filter(is_trashed=True).update(cost=None, remaining_worth=None)
-        self.stdout.write('Zero-ed %s deleted tasks cost and remaining_worth...' % str(count))
+            # zero cost
+            count = Task.objects.filter(is_trashed=True).update(cost=None, remaining_worth=None)
+            self.stdout.write('Zero-ed %s deleted tasks cost and remaining_worth...' % str(count))
 
-        self.stdout.write(self.style.SUCCESS('Successfully deleted all inputs to/from deleted tasks. Yay!'))
+            self.stdout.write(self.style.SUCCESS('Successfully deleted all inputs to/from deleted tasks. Yay!'))
+        except Exception as e:
+            self.stdout.write('__error__')
+            self.stdout.write(e)
+
+        self.stdout.write('All done.')
